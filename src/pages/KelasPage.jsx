a9f55fc285a972816bd9de2b202bccadaff02b58
Xenna <bagusgaming633@gmail.com>
2025-08-27 10:03:35 +0700

    KelasPage.jsx

--- src/pages/KelasPage.jsx
+++ src/pages/KelasPage.jsx
@@ -6,50 +6,65 @@ import FaqComponent from "../components/FaqComponent";
 const KelasPage = () => {
   return (
     <div className="kelas-page">
-    <div className="kelas min-vh-100">
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="fw-bold text-center animate__animated animate__fadeInUp animate__delay-1s" >Daftar Kereta</h1>
-            <p className="text-center animate__animated animate__fadeInUp animate__delay-1s">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, doloremque.
-              Impedit sint voluptatibus earum aspernatur, consectetur reprehenderit quo?
-              Provident nobis iste maiores, voluptas sequi error doloremque nostrum vel.
-              Fugiat, unde.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-           {semuaKelas.map((kelas) => (
-                        <Col key={kelas.id} className="mb-4 s shadow rounded" data-aos="fade-up" data-aos-duration="1000" data-aos-delay={kelas.delay}>
-                          <div className="bg-white rounded shadow-sm">
-                            <img
-                              src={kelas.image}
-                              alt="unsplash.com"
-                              className="w-100 mb-3 rounded-top"
-                            />
-                            <div className="star mb-2 px-3">
-                              <i className={kelas.star1}></i>
-                              <i className={kelas.star2}></i>
-                              <i className={kelas.star3}></i>
-                              <i className={kelas.star4}></i>
-                              <i className={kelas.star5}></i>
-                            </div>
-                            <h5 className="mb-3 px-3">{kelas.title}</h5>
-                            <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
-                              <p className="mb-0 text-primary fw-bold">{kelas.price}</p>
-                              <button className="btn btn-danger rounded-1">{kelas.buy}</button>
-                            </div>
-                          </div>
-                        </Col>
-                      ))}
-        </Row>
-      </Container>
-    </div>
+      <div className="kelas min-vh-100">
+        <Container>
+          {/* Header */}
+          <Row>
+            <Col>
+              <h1 className="fw-bold text-center animate__animated animate__fadeInUp animate__delay-1s">
+                Daftar Kereta
+              </h1>
+              <p className="text-center animate__animated animate__fadeInUp animate__delay-1s">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, doloremque.
+                Impedit sint voluptatibus earum aspernatur, consectetur reprehenderit quo?
+                Provident nobis iste maiores, voluptas sequi error doloremque nostrum vel.
+                Fugiat, unde.
+              </p>
+            </Col>
+          </Row>
 
-    <FaqComponent></FaqComponent>
-    </div>
+          {/* Cards */}
+          <Row>
+            {semuaKelas.map((kelas) => (
+              <Col
+                key={kelas.id}
+                md={4}
+                className="mb-4"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay={kelas.delay}
+              >
+                <div className="anime-card">
+                  {/* Gambar */}
+                  <img src={kelas.image} alt={kelas.title} />
 
+                  {/* Rating Bintang */}
+                  <div className="star mb-2 px-3 mt-2">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+
+                  {/* Judul */}
+                  <h5 className="px-3">{kelas.title}</h5>
+
+                  {/* Info Harga & Tombol */}
+                  <div className="anime-info">
+                    <p>{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">{kelas.buy}</button>
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+
+      {/* FAQ Section */}
+      <FaqComponent />
+    </div>
   );
 };
 
